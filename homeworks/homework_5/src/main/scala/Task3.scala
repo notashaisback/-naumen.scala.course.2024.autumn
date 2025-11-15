7a826b46fa97d4e16ad7190e573f1d7775f8406f
notashaisback <mnatali7772024@gmail.com>
2025-11-15 17:25:24 +0500

    Update Task3.scala

--- homeworks/homework_5/src/main/scala/Task3.scala
+++ homeworks/homework_5/src/main/scala/Task3.scala
@@ -25,8 +25,31 @@ object Task3 extends App {
   case class Count(word: String, count: Int)
   case class WordsCount(count: Seq[Count])
   object WordsCount {
-    implicit val monoid: Monoid[WordsCount] = ???
+    implicit val monoid: Monoid[WordsCount] = new Monoid[WordsCount] {
+      def empty: WordsCount = WordsCount(Seq.empty)
+
+      def combine(a: WordsCount, b: WordsCount): WordsCount = {
+        val combinedMap = (a.count ++ b.count)
+          .groupBy(_.word)
+          .map { case (word, counts) =>
+            Count(word, counts.map(_.count).sum)
+          }
+          .toSeq
+        WordsCount(combinedMap)
+      }
+    }
   }
 
-  def countWords(lines: Vector[String]): WordsCount = ???
+  def countWords(lines: Vector[String]): WordsCount = {
+    val futureResult = mapReduce(lines) { line =>
+      val wordCounts = line.split("\\s+")
+        .filter(_.nonEmpty)
+        .groupBy(identity)
+        .map { case (word, occurrences) => Count(word, occurrences.length) }
+        .toSeq
+      WordsCount(wordCounts)
+    }
+
+    Await.result(futureResult, 10.seconds)
+  }
 }
